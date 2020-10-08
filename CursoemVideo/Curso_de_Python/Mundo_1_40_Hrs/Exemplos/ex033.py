731ad788593971dfb92ed151bb04bc23c0edf4aa
Diego Barbosa <diegohabarbosa@gmail.com>
2020-10-08 02:05:37 -0300

    Update ex033.py

--- CursoemVideo/Curso_de_Python/Mundo_1_40_Hrs/Exemplos/ex033.py
+++ CursoemVideo/Curso_de_Python/Mundo_1_40_Hrs/Exemplos/ex033.py
@@ -2,6 +2,6 @@
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite um número: '))
 n3 = int(input('Digite um número: '))
-nmax = (max(n1,n2,n3))
-nmin = (min(n1,n2,n3))
-print('O número {} é o maior e o número {} é o menor'.format(nmax, nmin))
+nmax = max(n1,n2,n3)
+nmin = min(n1,n2,n3)
+print('O número {} é o maior e o número {} é o menor'.format(nmax, nmin))45

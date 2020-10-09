4a03c960d734c3972ba2bc5330541b749434402a
Diego Barbosa <diegohabarbosa@gmail.com>
2020-10-09 00:21:44 -0300

    Update ex0034

--- CursoemVideo/Curso_de_Python/Mundo_1_40_Hrs/Exemplos/ex034.py
+++ CursoemVideo/Curso_de_Python/Mundo_1_40_Hrs/Exemplos/ex034.py
@@ -1,4 +1,4 @@
-# Aumentos múltiplos
+# Aumentos múltiplos de salário
 s = float(input('Digite o sálario do colaborador: '))
 if s<=1250.00:
     print('O salário terá um reajuste de 15% e valor reajustado será de R${:.2f}'.format(s*1.15))

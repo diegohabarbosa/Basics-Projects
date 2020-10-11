b4fd790ac6b31b531b36338febc91451d059da6d
Diego Barbosa <diegohabarbosa@gmail.com>
2020-10-10 21:18:56 -0300

    Update ex031.py

--- CursoemVideo/Curso_de_Python/Mundo_1_40_Hrs/Exemplos/ex031.py
+++ CursoemVideo/Curso_de_Python/Mundo_1_40_Hrs/Exemplos/ex031.py
@@ -1,4 +1,4 @@
-# Custo da viagem
+# Custo da viagem por km
 km = float(input('Digite a distância do percurso da sua viagem em km: '))
 if km <= 200:
     print('O preço da sua passagem será R${:.2f}'.format(km*0.50))

9a268b40151127b6d81e6e6b5983737231f5b7be
Diego Barbosa <diegohabarbosa@gmail.com>
2020-10-13 03:58:05 -0300

    Exercícios 44 e 45

--- CursoemVideo/Curso_de_Python/Mundo_1_40_Hrs/Exemplos/Mundo2/Exercícios/ex044.py
+++ CursoemVideo/Curso_de_Python/Mundo_1_40_Hrs/Exemplos/Mundo2/Exercícios/ex044.py
@@ -1,1 +1,22 @@
 #Gerenciador de Pagamentos
+from time import sleep
+valor = float(input('Digite o valor do produto R$'))
+print(' '*20)
+print('Digite o digito da condição de pagamento sendo:')
+print('1 - dinheiro / cheque')
+print('2 - à vista no cartão')
+print('3 - em até 2x no cartão de crédito')
+print('4 - 3x ou mais no cartão de crédito')
+print(' '*20)
+sleep(5)
+condicao = int(input('Escolha a condição de pagamento (1 à 4): '))
+if condicao == 1:
+    print('Você receberá um desconto de 10% nesta compra no valor total de R${:.2f}'.format(valor*0.9))
+elif condicao == 2:
+    print('Você receberá um desconto de 5% nesta compra no valor total de R${:.2f}'.format(valor*0.95))
+elif condicao == 3:
+    print('O valor total da compra foi de R${:.2f}'.format(valor))
+elif condicao == 4:
+    print('Será aplicado um juros de 20% nesta compra no valor total de R${:.2f}'.format(valor*1.2))
+else:
+    print('Opção invalida')
---
+++ CursoemVideo/Curso_de_Python/Mundo_1_40_Hrs/Exemplos/Mundo2/Exercícios/ex045.py
@@ -0,0 +1,16 @@
+#GAME Jokenpô: Pedra Papel e Tesoura
+import random
+from time import sleep
+print('Jokenpô...')
+print(' '*20)
+sleep(2)
+v = str(input('Escolha entre Pedra, Papel e Tesoura: ')).strip().title()
+print(' '*20)
+m = random.choice(['Pedra', 'Papel', 'Tesoura'])
+print('Máquina escolheu {} e você escolheu {}'.format(m,v))
+if m == 'Papel' and v == 'Pedra' or m == 'Tesoura' and v == 'Papel' or m == 'Pedra' and v == 'Tesoura':
+    print('Você perdeu!!! Tente novamente...')
+elif v == 'Papel' and m == 'Pedra' or v == 'Tesoura' and m == 'Papel' or v == 'Pedra' and m == 'Tesoura':
+    print('Parabéns!!! Você ganhou...')
+else:
+    print('Deu empate!!! Tente novamente...')

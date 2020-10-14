0d6023a8da09f954553d6f27c10154c55350bf64
Diego Barbosa <diegohabarbosa@gmail.com>
2020-10-13 23:55:12 -0300

    Update ex045.py

--- CursoemVideo/Curso_de_Python/Mundo_1_40_Hrs/Exemplos/Mundo2/Exercícios/ex045.py
+++ CursoemVideo/Curso_de_Python/Mundo_1_40_Hrs/Exemplos/Mundo2/Exercícios/ex045.py
@@ -4,9 +4,9 @@ from time import sleep
 print('Jokenpô...')
 print(' '*20)
 sleep(2)
+m = random.choice(['Pedra', 'Papel', 'Tesoura'])
 v = str(input('Escolha entre Pedra, Papel e Tesoura: ')).strip().title()
 print(' '*20)
-m = random.choice(['Pedra', 'Papel', 'Tesoura'])
 print('Máquina escolheu {} e você escolheu {}'.format(m,v))
 if m == 'Papel' and v == 'Pedra' or m == 'Tesoura' and v == 'Papel' or m == 'Pedra' and v == 'Tesoura':
     print('Você perdeu!!! Tente novamente...')

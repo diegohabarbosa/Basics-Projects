40a41648f65828b154b5c9f2adc45a14a53c6672
Diego Barbosa <diegohabarbosa@gmail.com>
2020-10-16 04:30:54 -0300

    ex064 e ex065

--- CursoemVideo/Curso_de_Python/Mundo_1_40_Hrs/Exemplos/Mundo2/Exercícios/ex064.py
+++ CursoemVideo/Curso_de_Python/Mundo_1_40_Hrs/Exemplos/Mundo2/Exercícios/ex064.py
@@ -1,1 +1,10 @@
 #064 - Tratando vários valores v1.0
+b = 0
+c = 0
+s = 0
+print('Digite os números inteiros, caso queira encerrar digite 999')
+while s != 999:
+    s = int(input('Digite um número inteiro: '))
+    b += s 
+    c += 1
+print('A soma total foi de {} e foi digitado {} vezes.'.format(b-999,c-1))
---
+++ CursoemVideo/Curso_de_Python/Mundo_1_40_Hrs/Exemplos/Mundo2/Exercícios/ex065.py
@@ -0,0 +1,7 @@
+#065 - Maior e Menor valores
+while p >= d:
+    
+    x = str(input('Deseja continuar? [S/N]: ')).strip().upper()[0]         
+    if x == 'S':    
+
+    break

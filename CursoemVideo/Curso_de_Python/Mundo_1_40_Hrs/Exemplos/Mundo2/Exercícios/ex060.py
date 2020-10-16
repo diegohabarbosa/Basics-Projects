1111384dc93f7307d84d3f1cff9be43700d35bfd
Diego Barbosa <diegohabarbosa@gmail.com>
2020-10-16 03:07:16 -0300

    Update ex060.py

--- CursoemVideo/Curso_de_Python/Mundo_1_40_Hrs/Exemplos/Mundo2/Exercícios/ex060.py
+++ CursoemVideo/Curso_de_Python/Mundo_1_40_Hrs/Exemplos/Mundo2/Exercícios/ex060.py
@@ -1,1 +1,9 @@
 #060 - Cálculo do Fatorial
+n2 = 1
+n = int(input('Digite um número: '))
+print('{}! = '.format(n), end='')
+while 1 < n:
+    print(n, end='x')
+    n2 = n2*n
+    n -= 1
+print('{} = {}'.format(n, n2))

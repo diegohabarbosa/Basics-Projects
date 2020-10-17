860e536be7154b2a9e8a4b0d52d9707dce699f35
Diego Barbosa <diegohabarbosa@gmail.com>
2020-10-17 01:27:21 -0300

    update ex063 to ex065

--- CursoemVideo/Curso_de_Python/Mundo_1_40_Hrs/Exemplos/Mundo2/Exercícios/ex063.py
+++ CursoemVideo/Curso_de_Python/Mundo_1_40_Hrs/Exemplos/Mundo2/Exercícios/ex063.py
@@ -1,1 +1,12 @@
-#063 - Sequência de Fibonacci v1.0
+#063 - Sequência de Fibonacci v1.0 -- Fn = Fn - 1 + Fn - 2
+a = 0
+p = 0
+n = int(input('Digite o número de sequência: '))
+n2 = n**2
+while p < n2:
+    print(p, end=', ')
+    p = p + a
+    a = p - a
+    if p == 0:
+        p = p + 1
+print(p)
--- CursoemVideo/Curso_de_Python/Mundo_1_40_Hrs/Exemplos/Mundo2/Exercícios/ex065.py
+++ CursoemVideo/Curso_de_Python/Mundo_1_40_Hrs/Exemplos/Mundo2/Exercícios/ex065.py
@@ -1,7 +1,25 @@
 #065 - Maior e Menor valores
-while p >= d:
-    
-    x = str(input('Deseja continuar? [S/N]: ')).strip().upper()[0]         
-    if x == 'S':    
-
-    break
+b = 0
+c = 0
+ma = 0
+mi = 0
+s = int(input('Digite um número inteiro: '))
+while s != 'S':
+    b += s 
+    c += 1
+    if s > ma:
+        ma = s
+    elif s < mi:
+        mi = s           
+    x = str(input('Deseja continuar? [S/N]: ')).strip().upper()[0]
+    if x == 'S':
+        s = int(input('Digite um número inteiro: '))
+        b += s 
+        c += 1
+        if s > ma:
+                ma = s
+        elif s < mi:
+                mi = s
+        break       
+print('A média total dos valores foi de {}.'.format(b/(c-1)))
+print('O {} é o menor valor e o {} é o maior valor'.format(mi, ma))

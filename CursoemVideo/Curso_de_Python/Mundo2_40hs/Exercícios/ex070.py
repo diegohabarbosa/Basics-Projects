c2c04df8885293e62c04f4e71d38feea9f058fe1
Diego Barbosa <diegohabarbosa@gmail.com>
2020-10-19 04:20:55 -0300

    update ex070 e ex071

--- CursoemVideo/Curso_de_Python/Mundo2_40hs/Exercícios/ex070.py
+++ CursoemVideo/Curso_de_Python/Mundo2_40hs/Exercícios/ex070.py
@@ -1,1 +1,28 @@
 #070 - Estatísticas em produtos
+cont = soma = quant = 0
+chave = 'x'
+continuar = 'x'
+print('--'*20)
+print('         LOJA SUPER BARATÃO          ')
+print('--'*20)
+while continuar not in 'Nn':
+    produto = str(input('Nome do Produto: ')).strip().title()
+    preco = float(input('Preço: R$'))
+    soma += preco
+    quant += 1
+    if preco >= 1000:
+        cont += 1
+    if quant == 1:
+        menor = preco
+    else:
+        if preco < menor:
+            menor = preco
+            chave = produto
+    continuar = 'x'
+    while continuar not in 'SsNn':
+        continuar = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+print('')
+print('-------------- FIM DO PROGRAMA --------------')
+print(f'O total da compra foi R${soma:.2f}')
+print(f'Temos {cont} produtos custando mais de R$1000.00')
+print(f'O produto mais barato foi {chave} que custa {menor:.2f}')
--- CursoemVideo/Curso_de_Python/Mundo2_40hs/Exercícios/ex071.py
+++ CursoemVideo/Curso_de_Python/Mundo2_40hs/Exercícios/ex071.py
@@ -1,1 +1,8 @@
 #071 - Simulador de Caixa Eletrônico
+print('=='*20)
+print('             BANCO DO CEV                ')
+print('=='*20)
+
+valor = int(input('Que valor você deseja sacar? R$'))
+
+print('Volte sempre ao BANCO DO CEV! Tenha um bom dia!')

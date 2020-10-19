b4389b94dc90b262888c589817d754c35df3ff6c
Diego Barbosa <diegohabarbosa@gmail.com>
2020-10-18 23:35:03 -0300

    Update ex057.py

--- CursoemVideo/Curso_de_Python/Mundo_1_40_Hrs/Exemplos/Mundo2/Exercícios/ex057.py
+++ CursoemVideo/Curso_de_Python/Mundo_1_40_Hrs/Exemplos/Mundo2/Exercícios/ex057.py
@@ -2,4 +2,8 @@
 sexo = str(input('Informe o seu sexo [F/M]: ')).strip().upper()[0]
 while sexo not in 'MmFf':
     sexo = str(input('Opção Invalida! Informe o seu sexo [F/M]: ')).strip().upper()[0]
+if sexo == 'M':
+    sexo = 'masculino'
+elif sexo == 'F':
+    sexo = 'feminino'
 print('Sexo {} regisitrado com sucesso!'.format(sexo))

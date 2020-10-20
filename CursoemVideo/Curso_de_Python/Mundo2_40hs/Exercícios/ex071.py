e478158f021056ec5e149a2497400141ab312e16
Diego Barbosa <diegohabarbosa@gmail.com>
2020-10-19 23:50:03 -0300

    Update ex071

--- CursoemVideo/Curso_de_Python/Mundo2_40hs/Exercícios/ex071.py
+++ CursoemVideo/Curso_de_Python/Mundo2_40hs/Exercícios/ex071.py
@@ -1,11 +1,25 @@
 #071 - Simulador de Caixa Eletrônico
-cedula = 0
-print('=='*20)
-print('             BANCO DO CEV                ')
-print('=='*20)
-
+print('='*40)
+print('{:^40}'.format('BANCO DO CEV'))
+print('='*40)
 valor = int(input('Que valor você deseja sacar? R$'))
-    
-    #print(f'Total de {cedula} de R$50')
-
+total = valor
+cedula = 50
+totalcedulas = 0
+while True:
+    if total >= cedula:
+        total -= cedula    
+        totalcedulas += 1
+    else:
+        if totalcedulas > 0:
+            print(f'Total de {totalcedulas} Cédulas de R${cedula},00')
+        if cedula == 50:
+            cedula = 20
+        elif cedula == 20:
+            cedula = 10
+        elif cedula == 10:
+            cedula = 2
+        totalcedulas = 0
+        if total == 0:
+            break          
 print('Volte sempre ao BANCO DO CEV! Tenha um bom dia!')

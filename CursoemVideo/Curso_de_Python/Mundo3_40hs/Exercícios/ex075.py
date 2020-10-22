d2b016a118d8c525bbbe5040643f55728e10e817
Diego Barbosa <diegohabarbosa@gmail.com>
2020-10-21 23:01:26 -0300

    update ex075

--- CursoemVideo/Curso_de_Python/Mundo3_40hs/Exercícios/ex075.py
+++ CursoemVideo/Curso_de_Python/Mundo3_40hs/Exercícios/ex075.py
@@ -1,1 +1,16 @@
 #075 - Análise de dados em uma Tupla
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+n3 = int(input('Digite mais um número: '))
+n4 = int(input('Digite o último número: '))
+nt = (n1, n2, n3, n4)
+print(f'Você digitou os valores {nt}')
+print(f'O valor 9 apareceu {nt.count(9)} vezes')
+if nt.count(3) == 0:
+    print('O valor 3 não foi digitado em nenhuma posição')
+else:
+    print(f'O valor 3 apareceu {nt.count(3)} vezes')
+print(f'Os valores pares digitados foram ',end='')
+for c in nt:
+    if c % 2 == 0:
+        print(c, end=' ')
--- CursoemVideo/Curso_de_Python/Mundo3_40hs/Exercícios/ex076.py
+++ CursoemVideo/Curso_de_Python/Mundo3_40hs/Exercícios/ex076.py
@@ -1,1 +1,4 @@
 #076 - Lista de Preços com Tupla
+palavras = ('aprender','programar','linguagem','python','curso','gratis','estudar','praticar','trabalhar','mercado','programador','futuro')
+
+

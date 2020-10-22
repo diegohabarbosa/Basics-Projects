f987f89b5ed80704a8e08a21ac30e7da197a1752
Diego Barbosa <diegohabarbosa@gmail.com>
2020-10-21 23:31:54 -0300

    Update ex076.py

--- CursoemVideo/Curso_de_Python/Mundo3_40hs/Exercícios/ex076.py
+++ CursoemVideo/Curso_de_Python/Mundo3_40hs/Exercícios/ex076.py
@@ -1,4 +1,7 @@
 #076 - Lista de Preços com Tupla
 palavras = ('aprender','programar','linguagem','python','curso','gratis','estudar','praticar','trabalhar','mercado','programador','futuro')
+vogais = ('a', 'e', 'i', 'o', 'u')
 
-
+for c in palavras:
+    print('Na palavra ',end='')
+    print('{}'.format(c).upper())

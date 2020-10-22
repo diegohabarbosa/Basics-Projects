d41db67fd1ffa02faf9203ec375bc716bc5d896f
Diego Barbosa <diegohabarbosa@gmail.com>
2020-10-22 00:42:59 -0300

    Revisão

--- CursoemVideo/Curso_de_Python/Mundo3_40hs/Exercícios/ex072.py
+++ CursoemVideo/Curso_de_Python/Mundo3_40hs/Exercícios/ex072.py
@@ -3,5 +3,8 @@ numero = 'x'
 conflito = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
 while numero not in conflito:
     numero = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= numero <= 20:
+        break
+    print('Tente novamente. ',end='')
 extenso = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
 print(f'Você digitou o número {extenso[numero]}')
--- CursoemVideo/Curso_de_Python/Mundo3_40hs/Exercícios/ex076.py
+++ CursoemVideo/Curso_de_Python/Mundo3_40hs/Exercícios/ex076.py
@@ -1,7 +1,8 @@
 #076 - Lista de Preços com Tupla
 palavras = ('aprender','programar','linguagem','python','curso','gratis','estudar','praticar','trabalhar','mercado','programador','futuro')
 vogais = ('a', 'e', 'i', 'o', 'u')
-
 for c in palavras:
-    print('Na palavra ',end='')
-    print('{}'.format(c).upper())
+    if c.find('a'):
+        print(c)
+    if c.find('b'):
+        print(c)

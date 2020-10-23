9e2e30553953e3e6d2fedcf2277c0d2588a581e6
Diego Barbosa <diegohabarbosa@gmail.com>
2020-10-22 22:15:58 -0300

    Update ex077.py

--- CursoemVideo/Curso_de_Python/Mundo3_40hs/Exercícios/ex077.py
+++ CursoemVideo/Curso_de_Python/Mundo3_40hs/Exercícios/ex077.py
@@ -1,6 +1,5 @@
 #077 - Contando vogais em Tupla
 palavras = ('aprender','programar','linguagem','python','curso','gratis','estudar','praticar','trabalhar','mercado','programador','futuro')
-vogais = ('a', 'e', 'i', 'o', 'u')
 for p in palavras:
     print(f'\nNa palavra {p.upper()} temos ', end='')
     for letra in p:
